bf6c19a526a58b21a9905e8546a5f7747b13e319
Clownstantine <constantin2033@gmail.com>
2022-02-24 20:55:53 +0300

    Level 1 Set up.
    Assets import, Animations state machine, player controlls.

---
+++ Assets/Scripts/CinemachineExtentionsController.cs
@@ -0,0 +1,19 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CinemachineExtentionsController : CinemachineExtension
+{
+    [SerializeField] private float _pixelPerUnit = 32f;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            Vector3 finalPos = state.FinalPosition;
+            Vector3 roundedFinalPos = new Vector3(Round(finalPos.x), Round(finalPos.y), finalPos.z);
+            state.PositionCorrection += roundedFinalPos - finalPos;
+        }
+    }
+
+    private float Round(float value) => Mathf.Round(value * _pixelPerUnit) / _pixelPerUnit  ;
+}
